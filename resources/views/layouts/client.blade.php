1bbab8105286c9258b8312082e42f12faeb37805
Đỗ Hồng Quân <quandohong28@192.168.0.102>
2024-09-22 21:46:29 +0700

    feat: init project

---
+++ resources/views/layouts/client.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    {{-- OpenSan --}}
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+        rel="stylesheet">
+    {{-- Dancing Script  --}}
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet">
+    {{-- Berkshire Swash --}}
+    <link
+        href="https://fonts.googleapis.com/css2?family=Alex+Brush&family=Berkshire+Swash&family=Dancing+Script:wght@400..700&display=swap"
+        rel="stylesheet">
+    {{-- Vujahday Script --}}
+    <link
+        href="https://fonts.googleapis.com/css2?family=Alex+Brush&family=Berkshire+Swash&family=Dancing+Script:wght@400..700&family=Vujahday+Script&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+
+</body>
+
+</html>
