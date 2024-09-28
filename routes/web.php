597b80aec674cb487c3d1a007fbf2be820ed8282
hatt0904 <tranthihaaaa9423@gmail.com>
2024-09-28 09:51:46 +0700

    Thêm giao diện footer

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('clients.home');
 });
+
 // auth-login
 Route::get('/login', function () {
     return view('auths.login');
