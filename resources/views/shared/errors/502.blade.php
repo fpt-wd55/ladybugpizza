3bb1ba926ac25af647bc7de140daab1e85c3f413
Đỗ Hồng Quân <quandohong28@os-MacBook-Pro.local>
2024-09-28 09:39:29 +0700

    feat: tối ưu giao diện các trang lỗi

--- resources/views/shared/errors/502.blade.php
+++ resources/views/shared/errors/502.blade.php
@@ -3,14 +3,15 @@
 @section('title', '502 Cổng xấu')
 
 @section('content')
-<body class="bg-gray-100">
-    <div class="flex items-center justify-center min-h-screen bg-gray-100">
-        <div class="text-center p-2">
-            <h1 class="text-6xl md:text-9xl font-bold text-gray-800">502</h1>
-            <p class="text-2xl md:text-4xl font-medium text-gray-500 mt-6">Cổng xấu</p>
-            <p class="text-lg md:text-xl text-gray-400 mt-2">phản hồi không hợp lệ</p>
-            <a href="#" class="inline-block mt-12 bg-black px-6 py-2 text-white rounded-md shadow-lg uppercase">Trở về trang chủ</a>
+
+    <div class="bg-gray-100">
+        <div class="flex items-center justify-center min-h-screen bg-gray-100">
+            <div class="text-center p-2">
+                <h1 class="text-6xl md:text-9xl font-bold text-gray-800 mb-8">502</h1>
+                <p class="text-2xl md:text-4xl font-medium text-gray-500 mb-4">Bad Gateway</p>
+                <p class="text-sm md:text-xl font-medium text-gray-500 mb-8">Phản hồi không hợp lệ</p>
+                <a href="{{ route('client.home') }}" class="button-red">Trang chủ</a>
+            </div>
         </div>
     </div>
-</body>
 @endsection
