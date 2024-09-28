b88bf87996c4bbdccb2c118657b6b5a264d1911e
Cactus137 <Anhdo13072004@gmail.com>
2024-09-28 09:37:50 +0700

    example

--- resources/views/clients/home.blade.php
+++ resources/views/clients/home.blade.php
@@ -4,26 +4,12 @@
 
 @section('content')
     @session('success')
-    <div class="alert-success">
-        {{ session('success') }}
-    </div>
+        <div class="alert-success">
+            {{ session('success') }}
+        </div>
     @endsession
 
-    <div class="md:mx-32 lg:mx-64 border-black border-2 min-h-screen p-4 md:p-8">
-        <div class="grid grid-cols-2">
-            <div>
-                day la bo loc
-            </div>
-            <div>
-                <div></div>
-                <div>
-                    <p></p>
-                    <div class="grid grid-cols-2">
-                <div></div>
-                <div></div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="md:mx-32 lg:mx-64 min-h-screen p-4 md:p-8">
+
     </div>
 @endsection
