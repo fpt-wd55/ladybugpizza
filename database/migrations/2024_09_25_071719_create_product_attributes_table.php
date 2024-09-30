8d89c89f742672b025276957fba3a8fefaa174b7
Đỗ Hồng Quân <quandohong28@192.168.0.101>
2024-09-30 20:48:41 +0700

    commit tạm thời

--- database/migrations/2024_09_25_071719_create_product_attributes_table.php
+++ database/migrations/2024_09_25_071719_create_product_attributes_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->foreignId('attribute_value_id')->constrained()->cascadeOnDelete();
             $table->string('price');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
