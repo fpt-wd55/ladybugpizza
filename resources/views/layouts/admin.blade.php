f8807e2926c090d68b770022d19a4144a845692c
Cactus137 <Anhdo13072004@gmail.com>
2024-09-30 10:32:34 +0700

    Tao giao dien dardboard va table, form

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -21,20 +21,48 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Alex+Brush&family=Berkshire+Swash&family=Dancing+Script:wght@400..700&family=Vujahday+Script&display=swap"
         rel="stylesheet">
+    {{-- ApexCharts Script --}}
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 </head>
 
 <body class="text-sm">
-    
+    {{-- header --}}
     @include('partials.admins.header')
-
+    {{-- sidebar --}}
     @include('partials.admins.sidebar')
 
-    <div class="p-4 mt-20 sm:ml-64">
-
-        <div class="bg-white rounded-lg shadow p-4 min-h-screen">
+    <div class="mt-20 sm:ml-64">
+        {{-- breadcrumb --}}
+        <nav class="mb-4 flex px-4 pt-2" aria-label="Breadcrumb">
+            <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                <li class="inline-flex items-center">
+                    <a href="#"
+                        class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-primary-600">
+                        @svg('tabler-home', 'h-4 w-4 text-gray-400 me-2')
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <div class="flex items-center">
+                        @svg('tabler-chevron-right', 'h-4 w-4 text-gray-400 me-2')
+                        <a href="#"
+                            class="ms-1 text-sm font-medium text-gray-700 hover:text-primary-600 md:ms-2">My
+                            account</a>
+                    </div>
+                </li>
+                <li aria-current="page">
+                    <div class="flex items-center">
+                        @svg('tabler-chevron-right', 'h-4 w-4 text-gray-400 me-2')
+                        <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2">Account</span>
+                    </div>
+                </li>
+            </ol>
+        </nav>
+        {{-- content --}}
+        <div class="px-4">
             @yield('content')
         </div>
-
+        {{-- footer --}}
         @include('partials.admins.footer')
 
     </div>
