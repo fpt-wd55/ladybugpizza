24a8426f361cde1d4934b638f606d27aaa13fb48
quandohong28 <quandohong28@gmail.com>
2024-10-02 22:34:46 +0700

    Thêm mới giao diện trang giỏ hàng

--- resources/views/clients/cart/index.blade.php
+++ resources/views/clients/cart/index.blade.php
@@ -41,29 +41,47 @@
 
             <div class="mb-8">
                 <p class="title">GIỎ HÀNG</p>
-
-                <div class="product-card p-4">
-                    <button onclick="toggleAccordion(1)" class="w-full flex justify-between items-center mb-4">
-                        <div class="flex items-center gap-4">
-                            <img src="{{ asset('storage/uploads/products/pizza/pizza_tartufo.png') }}" class="img-lg rounded-lg"
-                            alt="">
-                        <p class="font-medium">Pizza xúc xích</p>
+                <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                    @for ($i = 0; $i < 5; $i++)
+                        <div class="product-card overflow-hidden">
+                            <div class="w-full flex justify-between items-center pe-4">
+                                <div class="flex gap-4">
+                                    <img src="{{ asset('storage/uploads/products/pizza/pizza_tartufo.png') }}" class="img-md"
+                                        alt="">
+                                    <div class="text-left py-2">
+                                        <p class="font-medium mb-4">Pizza xúc xích</p>
+                                        <div class="text-sm">
+                                            <p>Đế mỏng, size S</p>
+                                            <p>Topping: Thịt bò, cá hồi</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="text-right">
+                                    <div class="mb-4 text-sm flex items-center gap-2">
+                                        <span class="line-through">320,000đ</span>
+                                        <span class="font-medium">300,000đ</span>    
+                                    </div>
+                                    <div class="inline-flex rounded-md shadow-sm" role="group">
+                                        <button type="button"
+                                            class="px-2 py-1.5 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-red-500">
+                                            @svg('tabler-minus', 'icon-sm')
+                                        </button>
+                                        <div
+                                            class="px-4 py-1.5 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200">
+                                            1
+                                        </div>
+                                        <button type="button"
+                                            class="px-2 py-1.5 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-red-500">
+                                            @svg('tabler-plus', 'icon-sm')
+                                        </button>
+                                    </div>
+                                </div>
+                                <a href="#">
+                                    @svg('tabler-trash', 'icon-md text-red-600')
+                                </a>
+                            </div>
                         </div>
-                        <div class="flex items-center gap-4">
-                            <span id="icon-1" class="transition">
-                                @svg('tabler-eye', 'icon-md')
-                            </span>
-                            <a href="#">
-                                @svg('tabler-trash', 'icon-md text-red-600')
-                            </a>
-                        </div>
-                    </button>
-                    <div id="content-1" class="max-h-0 overflow-hidden transition">
-                        <div class=" text-sm text-slate-500">
-                            Material Tailwind is a framework that enhances Tailwind CSS with additional styles and
-                            components.
-                        </div>
-                    </div>
+                    @endfor
                 </div>
             </div>
             <div class="card p-4 md:p-8 mb-12">
@@ -93,17 +111,4 @@
             </div>
         </div>
     </div>
-
-    <script>
-        function toggleAccordion(index) {
-            const content = document.getElementById(`content-${index}`);
-
-            // Toggle the content's max-height for smooth opening and closing
-            if (content.style.maxHeight && content.style.maxHeight !== '0px') {
-                content.style.maxHeight = '0';
-            } else {
-                content.style.maxHeight = content.scrollHeight + 'px';
-            }
-        }
-    </script>
 @endsection
