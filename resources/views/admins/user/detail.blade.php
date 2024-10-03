b285c542f3311a49b0e090a120a0f2ad087e2f4c
Luca <blackwhilee04@gmail.com>
2024-10-03 11:23:15 +0700

    update: Sua svg

--- resources/views/admins/user/detail.blade.php
+++ resources/views/admins/user/detail.blade.php
@@ -83,13 +83,7 @@
                                                     <button type="button"
                                                         class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
                                                         data-modal-hide="default-modal">
-                                                        <svg class="w-3 h-3" aria-hidden="true"
-                                                            xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                            viewBox="0 0 14 14">
-                                                            <path stroke="currentColor" stroke-linecap="round"
-                                                                stroke-linejoin="round" stroke-width="2"
-                                                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                                                        </svg>
+                                                        @svg('tabler-x', 'w-4 h-4')
                                                         <span class="sr-only">Close modal</span>
                                                     </button>
                                                 </div>
@@ -214,13 +208,7 @@
                                                 <button type="button"
                                                     class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
                                                     data-modal-hide="add-address">
-                                                    <svg class="w-3 h-3" aria-hidden="true"
-                                                        xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                        viewBox="0 0 14 14">
-                                                        <path stroke="currentColor" stroke-linecap="round"
-                                                            stroke-linejoin="round" stroke-width="2"
-                                                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                                                    </svg>
+                                                    @svg('tabler-x', 'w-4 h-4')
                                                     <span class="sr-only">Close modal</span>
                                                 </button>
                                             </div>
