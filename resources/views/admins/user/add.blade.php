8fedb95d18a52db0dde7697060ab554f3cc6c465
Luca <blackwhilee04@gmail.com>
2024-10-03 10:52:28 +0700

    update: Sua giao dien form quan ly nguoi dung

--- resources/views/admins/user/add.blade.php
+++ resources/views/admins/user/add.blade.php
@@ -6,29 +6,45 @@
             <form action="{{ route('admin.users.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="grid gap-4 mb-4 sm:grid-cols-2">
-                    <div>
-                        <label for="username" class="block mb-2 text-sm font-medium text-gray-900 ">Tên tài khoản</label>
-                        <input type="text" name="username" id="username"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
-                        @error('username')
-                            <p class="mt-2 text-sm text-red-600 ">
-                                {{ $message }}
-                            </p>
-                        @enderror
-                    </div>
-                    <div>
-                        <label for="fullname" class="block mb-2 text-sm font-medium text-gray-900 ">Họ và tên</label>
-                        <input type="text" name="fullname" id="fullname"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
-                        @error('fullname')
-                            <p class="mt-2 text-sm text-red-600 ">
-                                {{ $message }}
-                            </p>
-                        @enderror
+                    <div class="sm:col-span-2">
+                        <div class="grid gap-4 mb-4 sm:grid-cols-3">
+                            <div>
+                                <label for="username" class="block mb-2 text-sm font-medium text-gray-900 ">Tên tài
+                                    khoản</label>
+                                <input type="text" name="username" id="username" placeholder="Tên tài khoản"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
+                                @error('username')
+                                    <p class="mt-2 text-sm text-red-600 ">
+                                        {{ $message }}
+                                    </p>
+                                @enderror
+                            </div>
+                            <div>
+                                <label for="fullname" class="block mb-2 text-sm font-medium text-gray-900 ">Họ và
+                                    tên</label>
+                                <input type="text" name="fullname" id="fullname" placeholder="Họ và tên"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
+                                @error('fullname')
+                                    <p class="mt-2 text-sm text-red-600 ">
+                                        {{ $message }}
+                                    </p>
+                                @enderror
+                            </div>
+                            <div>
+                                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Mật khẩu</label>
+                                <input type="password" name="password" id="password" placeholder="•••••••••••••"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
+                                @error('password')
+                                    <p class="mt-2 text-sm text-red-600 ">
+                                        {{ $message }}
+                                    </p>
+                                @enderror
+                            </div>
+                        </div>
                     </div>
                     <div>
                         <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Email</label>
-                        <input type="text" name="email" id="email"
+                        <input type="text" name="email" id="email" placeholder="email@domain.com"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
                         @error('email')
                             <p class="mt-2 text-sm text-red-600 ">
@@ -38,7 +54,7 @@
                     </div>
                     <div>
                         <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 ">Số điện thoại</label>
-                        <input type="text" name="phone" id="phone"
+                        <input type="text" name="phone" id="phone" placeholder="0123456789"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
                         @error('phone')
                             <p class="mt-2 text-sm text-red-600 ">
@@ -90,16 +106,40 @@
                         </div>
                     </div>
                     <div class="sm:col-span-2">
-                        <div class="grid gap-4 mb-4 sm:grid-cols-2">
+                        <div class="grid gap-4 mb-4 sm:grid-cols-3">
                             <div>
-                                <label for="roleSelect" class="block mb-2 text-sm font-medium text-gray-900 ">Vai
+                                <label for="status" class="block mb-4 text-sm font-medium text-gray-900 ">Trạng
+                                    thái</label>
+                                <label class="inline-flex items-center cursor-pointer">
+                                    <input type="checkbox" value="" name="status" class="sr-only peer">
+                                    <div
+                                        class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-0 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600">
+                                    </div>
+                                    <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Hoạt động</span>
+                                </label>
+                                @error('status')
+                                    <p class="mt-2 text-sm text-red-600 ">
+                                        {{ $message }}
+                                    </p>
+                                @enderror
+                            </div>
+                            <div>
+                                <label class="block mb-3 text-sm font-medium text-gray-900 ">Vai
                                     trò</label>
-                                <select id="roleSelect" name="roleSelect"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5">
-                                    <option selected>-- Vai trò --</option>
-                                    <option value="1">Quản trị viên</option>
-                                    <option value="2">Người dùng</option>
-                                </select>
+                                <div class="flex flex-wrap mb-2">
+                                    <div class="flex items-center mb-4 mr-4">
+                                        <input id="adminRole" type="radio" value="1" name="roleSelect"
+                                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-0 focus:outline-none">
+                                        <label for="adminRole" class="ms-2 text-sm font-medium text-gray-900">Quản trị
+                                            viên</label>
+                                    </div>
+                                    <div class="flex items-center mb-4 mr-4">
+                                        <input id="userRole" type="radio" value="2" name="roleSelect"
+                                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-0 focus:outline-none">
+                                        <label for="userRole" class="ms-2 text-sm font-medium text-gray-900">Người
+                                            dùng</label>
+                                    </div>
+                                </div>
                                 @error('roleSelect')
                                     <p class="mt-2 text-sm text-red-600 ">
                                         {{ $message }}
@@ -107,11 +147,11 @@
                                 @enderror
                             </div>
                             <div id="permissionSelect">
-                                <label for="category" class="block mb-2 text-sm font-medium text-gray-900 ">Quyền truy
-                                    cập</label>
+                                <label for="category" class="block mb-2 text-sm font-medium text-gray-900 ">Phân
+                                    quyền</label>
                                 <select name="permissionSelect"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5">
-                                    <option selected>-- Quyền truy cập --</option>
+                                    <option selected>-- Phân quyền --</option>
                                     <option value="3">Admin 1</option>
                                     <option value="4">Admin 2</option>
                                 </select>
@@ -122,30 +162,6 @@
                                 @enderror
                             </div>
                         </div>
-                    </div>
-                    <div>
-                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Mật khẩu</label>
-                        <input type="password" name="password" id="password"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
-                        @error('password')
-                            <p class="mt-2 text-sm text-red-600 ">
-                                {{ $message }}
-                            </p>
-                        @enderror
-                    </div>
-                    <div>
-                        <label for="status" class="block mb-2 text-sm font-medium text-gray-900 ">Trạng thái</label>
-                        <select name="status" id="status"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5">
-                            <option selected>-- Trạng thái --</option>
-                            <option value="1">Hoạt động</option>
-                            <option value="2">Khóa tài khoản</option>
-                        </select>
-                        @error('status')
-                            <p class="mt-2 text-sm text-red-600 ">
-                                {{ $message }}
-                            </p>
-                        @enderror
                     </div>
                     <div class="sm:col-span-2">
                         <div class="grid gap-4 mb-4 sm:grid-cols-3">
@@ -202,7 +218,7 @@
                     <div class="sm:col-span-2">
                         <label for="detail_address" class="block mb-2 text-sm font-medium text-gray-900 ">Địa chỉ chi
                             tiết</label>
-                        <input type="text" name="detail_address" id="detail_address"
+                        <input type="text" name="detail_address" id="detail_address" placeholder="Địa chỉ chi tiết"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
                         @error('detail_address')
                             <p class="mt-2 text-sm text-red-600 ">
@@ -227,14 +243,23 @@
 @endsection
 @section('scripts')
     <script>
-        const permissionSelect = document.getElementById('permissionSelect');
-        permissionSelect.style.display = 'none';
-        document.getElementById('roleSelect').addEventListener('change', function() {
-            if (this.value === '1') {
-                permissionSelect.style.display = 'block';
-            } else {
-                permissionSelect.style.display = 'none';
+        document.addEventListener('DOMContentLoaded', function() {
+            const adminRole = document.getElementById('adminRole');
+            const userRole = document.getElementById('userRole');
+            const permissionSelect = document.getElementById('permissionSelect');
+
+            function toggleForm() {
+                if (adminRole.checked) {
+                    permissionSelect.style.display = 'block';
+                } else {
+                    permissionSelect.style.display = 'none';
+                }
             }
+
+            adminRole.addEventListener('change', toggleForm);
+            userRole.addEventListener('change', toggleForm);
+
+            toggleForm();
         });
     </script>
 @endsection
