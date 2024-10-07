8854ba653e924f8809dd727a5fc344d553c5eb2f
Luca <blackwhilee04@gmail.com>
2024-10-07 09:30:44 +0700

    xay dung giao dien danh sach thuoc tinh

--- database/migrations/2024_09_25_065009_create_attribute_values_table.php
+++ database/migrations/2024_09_25_065009_create_attribute_values_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('value');
             $table->integer('quantity');
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
