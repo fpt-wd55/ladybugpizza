d7520b41e83625aa4e84db01d6c832834c289377
Do Hong Quan <quandohong28@gmail.com>
2024-10-12 00:44:36 +0700

    Thêm mới danh mục sản phẩm

--- resources/views/clients/categories.blade.php
+++ resources/views/clients/categories.blade.php
@@ -2,8 +2,13 @@
     $categories = \App\Models\Category::all();
 @endphp
 
-<div class="flex flex-wrap items-center gap-4 mb-4 md:mb-8">
+<div class="flex flex-wrap items-center justify-between gap-4 mb-4 md:mb-8">
     @foreach ($categories as $category)
-        {{ $category->name }}
+        <a href="{{ route('client.product.menu', ['category' => $category->slug]) }}"
+            class="flex flex-col items-center justify-center">
+            <img src="{{ asset('storage/uploads/categories/' . $category->image) }}"
+                class="w-12 h-12 object-cover hover:transform hover:scale-110 transition-transform" alt="">
+            <p class="font-normal text-sm">{{ $category->name }}</p>
+        </a>
     @endforeach
 </div>
