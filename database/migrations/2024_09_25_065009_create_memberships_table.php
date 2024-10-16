dec2563eaa084b7715f1ec9e655b5d9953586792
hieukhocnhe <trantrunghieu422@gmail.com>
2024-10-16 21:04:46 +0700

    Cap nhat migration bang membership

--- database/migrations/2024_09_25_065009_create_memberships_table.php
+++ database/migrations/2024_09_25_065009_create_memberships_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->integer('points')->default(0);
+            $table->integer('total_spent')->default(0);
             $table->foreignId('rank_id')->constrained('membership_ranks')->onDelete('cascade');
             $table->timestamps();
         });
