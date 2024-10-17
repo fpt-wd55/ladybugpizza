7c7f1d34192368ec6fc1528ba6bd595fa6e45f2c
hatt0904 <tranthihaaaa9423@gmail.com>
2024-10-17 17:20:27 +0700

    Cập nhật chức năng trang

--- app/Http/Controllers/Client/PageController.php
+++ app/Http/Controllers/Client/PageController.php
@@ -8,7 +8,6 @@ use App\Http\Requests\ContactRequest;
 use App\Mail\Contact;
 use App\Mail\ThankYou;
 use App\Models\Notification;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
