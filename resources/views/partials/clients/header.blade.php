3613e31605385e4257278a756986392ceb66ed11
Do Hong Quan <quandohong28@gmail.com>
2024-10-21 16:39:36 +0700

    Thêm route chuyển hướng sang trang quản trị nếu user là admin

--- resources/views/partials/clients/header.blade.php
+++ resources/views/partials/clients/header.blade.php
@@ -15,38 +15,31 @@
                         <a href="{{ route('client.product.menu') }}">THỰC ĐƠN</a>
                     </li>
                     <li class="text-xs font-medium uppercase transition hover:text-red-500">
-                        <span class="flex items-center gap-1 hover:cursor-pointer"
-                            data-dropdown-toggle="aboutUsDropdown">
+                        <span class="flex items-center gap-1 hover:cursor-pointer" data-dropdown-toggle="aboutUsDropdown">
                             GIỚI THIỆU
                             @svg('tabler-chevron-down', 'w-4')
                         </span>
                     </li>
 
                     {{-- About us dropdown --}}
-                    <div class="z-10 hidden w-64 divide-y divide-gray-100 rounded-lg bg-white font-normal shadow"
-                        id="aboutUsDropdown">
+                    <div class="z-10 hidden w-64 divide-y divide-gray-100 rounded-lg bg-white font-normal shadow" id="aboutUsDropdown">
                         <div class="py-2">
-                            <a class="flex items-center gap-2 px-4 py-2 text-xs font-medium uppercase transition hover:bg-gray-100 hover:text-red-500"
-                                href="{{ route('client.about-us') }}">Về chúng tôi</a>
+                            <a class="flex items-center gap-2 px-4 py-2 text-xs font-medium uppercase transition hover:bg-gray-100 hover:text-red-500" href="{{ route('client.about-us') }}">Về chúng tôi</a>
                         </div>
                         <div class="py-2">
-                            <a class="flex items-center gap-2 px-4 py-2 text-xs font-medium uppercase transition hover:bg-gray-100 hover:text-red-500"
-                                href="{{ route('client.contact') }}">Liên hệ</a>
+                            <a class="flex items-center gap-2 px-4 py-2 text-xs font-medium uppercase transition hover:bg-gray-100 hover:text-red-500" href="{{ route('client.contact') }}">Liên hệ</a>
                         </div>
                         <div class="py-2">
-                            <a class="flex items-center gap-2 px-4 py-2 text-xs font-medium uppercase transition hover:bg-gray-100 hover:text-red-500"
-                                href="{{ route('client.manual') }}">Hướng dẫn mua hàng</a>
+                            <a class="flex items-center gap-2 px-4 py-2 text-xs font-medium uppercase transition hover:bg-gray-100 hover:text-red-500" href="{{ route('client.manual') }}">Hướng dẫn mua hàng</a>
                         </div>
                         <div class="py-2">
-                            <a class="flex items-center gap-2 px-4 py-2 text-xs font-medium uppercase transition hover:bg-gray-100 hover:text-red-500"
-                                href="{{ route('client.policies') }}">Chính sách và điều khoản</a>
+                            <a class="flex items-center gap-2 px-4 py-2 text-xs font-medium uppercase transition hover:bg-gray-100 hover:text-red-500" href="{{ route('client.policies') }}">Chính sách và điều khoản</a>
                         </div>
                     </div>
                 </div>
             </ul>
             <div class="flex items-center gap-4">
-                <button class="hidden md:inline-block" data-modal-target="searchModal"
-                    data-modal-toggle="searchModal">@svg('tabler-search', 'icon-md')</button>
+                <button class="hidden md:inline-block" data-modal-target="searchModal" data-modal-toggle="searchModal">@svg('tabler-search', 'icon-md')</button>
 
                 @if (Auth::user())
                     <a data-modal-target="favoriteProductModal" data-modal-toggle="favoriteProductModal" href="#">
@@ -55,55 +48,55 @@
                     <a href="{{ route('client.order.index') }}"> @svg('tabler-truck-delivery', 'icon-md')</a>
 
                     <button class="hover:cursor-pointer">
-                        <img class="img-circle h-8 w-8 object-cover" data-dropdown-toggle="userDropdown" loading="lazy"
-                            src="{{ Auth::user()->avatar() }}">
+                        <img class="img-circle h-8 w-8 object-cover" data-dropdown-toggle="userDropdown" loading="lazy" src="{{ Auth::user()->avatar() }}">
                     </button>
 
                     {{-- User dropdown --}}
-                    <div class="z-10 hidden w-64 divide-y divide-gray-100 rounded-lg bg-white font-normal shadow"
-                        id="userDropdown">
+                    <div class="z-10 hidden w-64 divide-y divide-gray-100 rounded-lg bg-white font-normal shadow" id="userDropdown">
                         <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
                             <p class="font-medium">{{ Auth::user()->fullname }}</p>
                             <p class="truncate">{{ Auth::user()->email }}</p>
                         </div>
+                        @if (Auth::user()->role_id == 3 || Auth::user()->role_id == 1)
+                            <div class="py-2">
+                                <a target="blank" class="flex items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" href="{{ route('admin.dashboard') }}">
+                                    @svg('tabler-external-link', 'icon-sm')
+                                    Trang quản trị
+                                </a>
+                            </div>
+                        @endif
                         <div class="py-2">
-                            <a class="flex items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                href="{{ route('client.profile.index') }}">
+                            <a class="flex items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" href="{{ route('client.profile.index') }}">
                                 @svg('tabler-user', 'icon-sm')
                                 Hồ sơ
                             </a>
                         </div>
                         <div class="py-2">
-                            <a class="flex items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                href="{{ route('client.profile.address') }}">
+                            <a class="flex items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" href="{{ route('client.profile.address') }}">
                                 @svg('tabler-location', 'icon-sm')
                                 Địa chỉ
                             </a>
                         </div>
                         <div class="py-2">
-                            <a class="flex items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                href="{{ route('client.profile.settings') }}">
+                            <a class="flex items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" href="{{ route('client.profile.settings') }}">
                                 @svg('tabler-settings', 'icon-sm')
                                 Cài đặt
                             </a>
                         </div>
                         <div class="py-2">
-                            <a class="flex items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                href="{{ route('client.profile.membership') }}">
+                            <a class="flex items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" href="{{ route('client.profile.membership') }}">
                                 @svg('tabler-coin', 'icon-sm')
                                 Tích điểm
                             </a>
                         </div>
                         <div class="py-2">
-                            <a class="flex items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                href="{{ route('client.profile.promotion') }}">
+                            <a class="flex items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" href="{{ route('client.profile.promotion') }}">
                                 @svg('tabler-tag', 'icon-sm')
                                 Mã giảm giá
                             </a>
                         </div>
                         <div class="py-2">
-                            <a class="flex items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                data-modal-target="logoutModal" data-modal-toggle="logoutModal" href="#">
+                            <a class="flex items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" data-modal-target="logoutModal" data-modal-toggle="logoutModal" href="#">
                                 @svg('tabler-logout', 'icon-sm')
                                 Đăng xuất
                             </a>
@@ -122,18 +115,14 @@
 </header>
 
 {{-- Search Modal --}}
-<div aria-hidden="true"
-    class="fixed left-0 right-0 top-0 z-50 hidden h-[calc(100%-1rem)] max-h-full w-full items-center justify-center overflow-y-auto overflow-x-hidden md:inset-0"
-    id="searchModal" tabindex="-1">
+<div aria-hidden="true" class="fixed left-0 right-0 top-0 z-50 hidden h-[calc(100%-1rem)] max-h-full w-full items-center justify-center overflow-y-auto overflow-x-hidden md:inset-0" id="searchModal" tabindex="-1">
     <div class="relative max-h-full w-full max-w-2xl p-4">
         <div class="relative rounded-lg bg-white shadow dark:bg-gray-700">
             <div class="flex items-center justify-between rounded-t border-b p-4 dark:border-gray-600 md:p-5">
                 <p class="font-semibold text-gray-900 dark:text-white">
                     Tìm kiếm
                 </p>
-                <button
-                    class="ms-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
-                    data-modal-hide="searchModal" type="button">
+                <button class="ms-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="searchModal" type="button">
                     @svg('tabler-x', 'icon-sm')
                 </button>
             </div>
@@ -153,16 +142,12 @@
     $favorites = Favorite::where('user_id', Auth::id())->with('product')->get();
 @endphp
 {{-- Favorite Product Modal --}}
-<div aria-hidden="true"
-    class="fixed left-0 right-0 top-0 z-50 hidden h-[calc(100%-1rem)] max-h-full w-full items-center justify-center overflow-y-auto overflow-x-hidden md:inset-0"
-    id="favoriteProductModal" tabindex="-1">
+<div aria-hidden="true" class="fixed left-0 right-0 top-0 z-50 hidden h-[calc(100%-1rem)] max-h-full w-full items-center justify-center overflow-y-auto overflow-x-hidden md:inset-0" id="favoriteProductModal" tabindex="-1">
     <div class="relative max-h-full w-full max-w-2xl p-4">
         <div class="relative rounded-lg bg-white shadow dark:bg-gray-700">
             <div class="flex items-center justify-between rounded-t border-b p-4 dark:border-gray-600 md:p-5">
                 <p class="font-semibold text-gray-900 dark:text-white">Sản phẩm yêu thích của tôi</p>
-                <button
-                    class="ms-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
-                    data-modal-hide="favoriteProductModal" type="button">
+                <button class="ms-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="favoriteProductModal" type="button">
                     @svg('tabler-x', 'icon-sm')
                 </button>
             </div>
@@ -170,11 +155,8 @@
             <div class="p-4 md:p-8">
                 <div class="mb-8 grid grid-cols-2 gap-4 lg:mb-16 lg:grid-cols-2">
                     @foreach ($favorites as $favorite)
-                        <a class="product-card overflow-hidden md:flex"
-                            href="{{ route('client.product.show', $favorite->product->slug) }}">
-                            <img alt="{{ $favorite->product->image }}"
-                                class="h-48 w-full flex-shrink-0 object-cover md:h-full md:w-1/3" loading="lazy"
-                                src="{{ asset('storage/uploads/products/' . $favorite->product->image) }}">
+                        <a class="product-card overflow-hidden md:flex" href="{{ route('client.product.show', $favorite->product->slug) }}">
+                            <img alt="{{ $favorite->product->image }}" class="h-48 w-full flex-shrink-0 object-cover md:h-full md:w-1/3" loading="lazy" src="{{ asset('storage/uploads/products/' . $favorite->product->image) }}">
                             <div class="p-2 text-sm">
                                 <p class="mb-2 font-semibold">{{ $favorite->product->name }}</p>
                                 <div class="mb-2 flex items-center gap-1">
@@ -189,7 +171,8 @@
                                 <p class="mb-4 line-clamp-3 h-12">{{ $favorite->product->description }}</p>
                                 <div class="bottom-4 flex items-center gap-3">
                                     <p class="text-xs text-gray-500 line-through">
-                                        {{ number_format($favorite->product->price) }}đ</p>
+                                        {{ number_format($favorite->product->price) }}đ
+                                    </p>
                                     <p class="font-semibold">{{ number_format($favorite->product->discount_price) }}đ
                                     </p>
                                 </div>
@@ -203,10 +186,8 @@
 </div>
 
 {{-- Logout Modal --}}
-<div aria-hidden="true"
-    class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
-    id="logoutModal" tabindex="-1">
-    <div class="relative w-full max-w-md max-h-full">
+<div aria-hidden="true" class="fixed left-0 right-0 top-0 z-50 hidden h-[calc(100%-1rem)] max-h-full w-full overflow-y-auto overflow-x-hidden p-4 md:inset-0" id="logoutModal" tabindex="-1">
+    <div class="relative max-h-full w-full max-w-md">
         <div class="relative rounded-lg bg-white p-4 shadow sm:p-5">
             <div class="mb-16 flex flex-col items-center justify-center gap-4 text-sm">
                 <div>@svg('tabler-alert-triangle', 'icon-2xl text-red-500')</div>
