e0dcbe70fde0b296fd1eee916cca0fee70acbb64
hatt0904 <tranthihaaaa9423gmail.com>
2024-10-22 09:43:19 +0700

    Cập nhât chức năng mới địac chỉ

--- resources/views/clients/profile/address/add.blade.php
+++ resources/views/clients/profile/address/add.blade.php
@@ -38,6 +38,7 @@
                         <p class="text-red-500 text-sm">{{ $message }}</p>
                     @enderror
                 </div>
+                
                 <div>
                     <label for="ward"
                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Xã/Phường:*</label>
