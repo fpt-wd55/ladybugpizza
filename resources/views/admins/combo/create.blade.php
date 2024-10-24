a084c568993040de3f0e07725ceaedc6e49d4a90
hieukhocnhe <trantrunghieu422@gmail.com>
2024-10-24 10:44:17 +0700

    Cập nhật giao diện thêm combo

--- resources/views/admins/combo/create.blade.php
+++ resources/views/admins/combo/create.blade.php
@@ -25,7 +25,7 @@
                             <label for="price" class="block mb-2 text-sm font-medium text-gray-900 ">Giá bán thường
                                 (đ)</label>
                             <input type="number" name="price" id="price" value="{{ old('price') }}"
-                                placeholder="Giá bán thường"
+                                placeholder="Giá bán thường" min="0"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
                             @error('price')
                                 <p class="mt-2 text-sm text-red-600 ">
@@ -37,7 +37,7 @@
                             <label for="discount_price" class="block mb-2 text-sm font-medium text-gray-900 ">Giá khuyến
                                 mãi (đ)</label>
                             <input type="number" name="discount_price" id="discount_price" placeholder="Giá khuyến mãi"
-                                value="{{ old('discount_price') }}"
+                                value="{{ old('discount_price') }}" min="0"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
                             @error('discount_price')
                                 <p class="mt-2 text-sm text-red-600 ">
@@ -72,7 +72,7 @@
                             <div>
                                 <label for="price" class="block mb-2 text-sm font-medium text-gray-900 ">Số lượng</label>
                                 <input type="number" name="price" id="price" value="{{ old('price') }}"
-                                    placeholder="Số lượng"
+                                    placeholder="Số lượng" min="0"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
                                 @error('price')
                                     <p class="mt-2 text-sm text-red-600 ">
@@ -148,70 +148,64 @@
                             </thead>
 
                             <tbody>
-                                @php
-                                    $key = 0;
-                                @endphp
-                                @if (request()->old('stocks'))
-                                    <tr>
-                                        <td class="border px-4 py-2" colspan="3">
-                                            <div class="grid grid-cols-1 gap-3 md:grid-cols-6">
-                                                <div class="md:col-span-3">
-                                                    <input type="text"
-                                                        name="stocks[{{ $key }}][attribute_value]"
-                                                        placeholder="Tên giá trị"
-                                                        value="{{ $stock['attribute_value'] ?? '' }}"
-                                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
-                                                    @error("stocks.{$key}.attribute_value")
-                                                        <p class="mt-2 text-sm text-red-600 ">
-                                                            {{ $message }}
-                                                        </p>
-                                                    @enderror
-                                                </div>
-                                                <div class="md:col-span-2">
-                                                    <input type="number"
-                                                        name="stocks[{{ $key }}][attribute_quantity]"
-                                                        placeholder="Số lượng"
-                                                        value="{{ $stock['attribute_quantity'] ?? '' }}"
-                                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
-                                                    @error("stocks.{$key}.attribute_quantity")
-                                                        <p class="mt-2 text-sm text-red-600 ">
-                                                            {{ $message }}
-                                                        </p>
-                                                    @enderror
-                                                </div>
-                                                <div class="md:col-span-1">
-                                                    <button type="button"
-                                                        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-0 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 btn-add-more-rm w-full">
-                                                        Xóa giá trị
-                                                    </button>
-                                                </div>
+                                <tr>
+                                    <td class="border px-4 py-2" colspan="3">
+                                        <h2 class="pb-2">Pizza</h2>
+                                        <div class="grid grid-cols-1 gap-3 md:grid-cols-8">
+                                            <div class="md:col-span-2">
+                                                <select name="" id="" class="input">
+                                                    <option value="">Chọn pizza</option>
+                                                </select>
                                             </div>
-                                        </td>
-                                    </tr>
-                                @else
-                                    <tr>
-                                        <td class="border px-4 py-2" colspan="3">
-                                            <div class="grid grid-cols-1 gap-3 md:grid-cols-6">
-                                                <div class="md:col-span-3">
-                                                    <input type="text" name="stocks[0][attribute_value]"
-                                                        placeholder="Tên giá trị"
-                                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
-                                                </div>
-                                                <div class="md:col-span-2">
-                                                    <input type="number" name="stocks[0][attribute_quantity]"
-                                                        placeholder="Số lượng"
-                                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
-                                                </div>
-                                                <div class="md:col-span-1">
-                                                    <button type="button"
-                                                        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-0 font-medium rounded-lg text-sm px-5 py-2.5 btn-add-more-rm w-full">
-                                                        Xóa giá trị
-                                                    </button>
-                                                </div>
+                                            <div class="md:col-span-2">
+                                                <select name="" id="" class="input">
+                                                    <option value="">Chọn thuộc tính</option>
+                                                </select>
                                             </div>
-                                        </td>
-                                    </tr>
-                                @endif
+                                            <div class="md:col-span-2">
+                                                <input type="number" name="" placeholder="Số lượng"
+                                                    class="input" min="0">
+                                            </div>
+                                            <div class="flex gap-3 md:col-span-2">
+                                                <button type="button" class="flex items-center button-blue">
+                                                    Thêm sản phẩm
+                                                </button>
+                                                <button type="button" class="flex items-center button-red">
+                                                    Xóa sản phẩm
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="border px-4 py-2" colspan="3">
+                                        <h2 class="pb-2">Bánh</h2>
+                                        <div class="grid grid-cols-1 gap-3 md:grid-cols-8">
+                                            <div class="md:col-span-2">
+                                                <select name="" id="" class="input">
+                                                    <option value="">Chọn pizza</option>
+                                                </select>
+                                            </div>
+                                            <div class="md:col-span-2">
+                                                <select name="" id="" class="input">
+                                                    <option value="">Chọn thuộc tính</option>
+                                                </select>
+                                            </div>
+                                            <div class="md:col-span-2">
+                                                <input type="number" name="" placeholder="Số lượng"
+                                                    class="input" min="0">
+                                            </div>
+                                            <div class="flex gap-3 md:col-span-2">
+                                                <button type="button" class="flex items-center button-blue">
+                                                    Thêm sản phẩm
+                                                </button>
+                                                <button type="button" class="flex items-center button-red">
+                                                    Xóa sản phẩm
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
