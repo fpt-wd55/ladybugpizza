26b41cb332685ff0f0b24681a0b8ad5f04933e8b
Huuduy2004 <duynguyenhuu2004@gmail.com>
2024-10-24 12:11:40 +0700

    Xây dựng chức năng xuất dữ liệu sản phẩm

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -206,64 +206,6 @@ class ProductController extends Controller
     public function export()
     {
         $products = Product::all();
-        $spreadsheet = new Spreadsheet();
-        $sheet = $spreadsheet->getActiveSheet();
-        $sheet->setCellValue('A1', 'ID');
-        $sheet->setCellValue('B1', 'Tên sản phẩm');
-        $sheet->setCellValue('C1', 'Slug');
-        $sheet->setCellValue('D1', 'Mô tả');
-        $sheet->setCellValue('E1', 'Danh mục');
-        $sheet->setCellValue('F1', "Giá ban đầu\n(VNĐ)");
-        $sheet->setCellValue('G1', "Giá khuyến mãi\n(VNĐ)");
-        $sheet->setCellValue('H1', 'Số lượng');
-        $sheet->setCellValue('I1', 'Mã sản phẩm');
-        $sheet->setCellValue('J1', "Trạng thái\n(1: Hoạt động, 2: Khóa)");
-        $sheet->setCellValue('K1', 'Nổi bật');
-        $sheet->setCellValue('L1', 'Đánh giá trung bình');
-        $sheet->setCellValue('M1', 'Tổng đánh giá');
-        $sheet->setCellValue('N1', 'Ngày xóa');
-        $sheet->setCellValue('O1', 'Ngày tạo');
-        $sheet->setCellValue('P1', 'Ngày cập nhật');
-        // In đậm tiêu đề
-        $sheet->getStyle('A1:P1')->getFont()->setBold(true);
-        // Tự động điều chỉnh độ rộng cột
-        foreach (range('A', 'P') as $columnID) {
-            $sheet->getColumnDimension($columnID)->setAutoSize(true);
-            $sheet->getStyle($columnID)->getAlignment()->setVertical('center');
-            $sheet->getStyle($columnID)->getAlignment()->setWrapText(true);
-        }
-
-        $row = 2;
-        foreach ($products as $product) {
-            $sheet->setCellValue('A' . $row, $product->id);
-            $sheet->setCellValue('B' . $row, $product->name);
-            $sheet->setCellValue('C' . $row, $product->slug);
-            $sheet->setCellValue('D' . $row, $product->description);
-            $sheet->setCellValue('E' . $row, $product->category->name);
-            // Giá là vietnamese đồng
-            $sheet->getStyle('F' . $row)->getNumberFormat()->setFormatCode('#,##0');
-            $sheet->getStyle('G' . $row)->getNumberFormat()->setFormatCode('#,##0');
-            $sheet->setCellValue('F' . $row, $product->price);
-            $sheet->setCellValue('G' . $row, $product->discount_price);
-            $sheet->setCellValue('H' . $row, $product->quantity);
-            $sheet->setCellValue('I' . $row, $product->sku);
-            $sheet->setCellValue('J' . $row, $product->status);
-            $sheet->setCellValue('K' . $row, $product->is_featured);
-            $sheet->setCellValue('L' . $row, $product->avg_rating);
-            $sheet->setCellValue('M' . $row, $product->total_rating);
-            $sheet->setCellValue('N' . $row, $product->deleted_at);
-            $sheet->setCellValue('O' . $row, $product->created_at);
-            $sheet->setCellValue('P' . $row, $product->updated_at);
-            $row++;
-        }
-
-        $writer = new Xlsx($spreadsheet);
-
-        header('Content-Type: application/vnd.ms-excel');
-        header('Content-Disposition: attachment;filename="danhsachsanpham.xlsx"');
-        header('Cache-Control: max-age=0');
-
-        $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
-        $writer->save('php://output');
+        $this->exportExcel($products, 'danhsachsanpham');
     }
 }
