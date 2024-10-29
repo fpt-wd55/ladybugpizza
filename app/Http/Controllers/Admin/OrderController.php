2ec95ca218074aea46e4b93b833cc1a72707f4b1
Luca <blackwhilee04@gmail.com>
2024-10-29 21:10:18 +0700

    update order

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -16,21 +16,21 @@ class OrderController extends Controller
      * Display a listing of the resource.
      */
     public function index(Request $request)
-{
-    $status = $request->get('tab'); // Change 'status' to 'tab'
-    
-    $orders = Order::when($status, function ($query, $status) {
-        return $query->whereHas('orderStatus', function ($q) use ($status) {
-            $q->where('slug', $status); // Ensure you're querying the 'slug' column
-        });
-    })->with('orderItems.productAttributes.product', 'orderItems.toppings')
-      ->latest('id')
-      ->paginate(10);
+    {
+        $status = $request->get('tab'); // Change 'status' to 'tab'
 
-    $orderStatuses = OrderStatus::all();
-    $invoices = Invoice::all();
-    return view('admins.order.index', compact('orders', 'invoices', 'orderStatuses'));
-}
+        $orders = Order::when($status, function ($query, $status) {
+            return $query->whereHas('orderStatus', function ($q) use ($status) {
+                $q->where('slug', $status); // Ensure you're querying the 'slug' column
+            });
+        })->with('orderItems.productAttributes.product', 'orderItems.toppings')
+            ->latest('id')
+            ->paginate(10); 
+            
+        $orderStatuses = OrderStatus::all();
+        $invoices = Invoice::all();
+        return view('admins.order.index', compact('orders', 'invoices', 'orderStatuses'));
+    }
 
 
     /**
@@ -77,8 +77,9 @@ class OrderController extends Controller
         $order->orderStatus->save();
 
         return redirect()->route('admin.orders.edit', $id)->with('success', 'Trạng thái đã được cập nhật!');
-    } 
-    public function export(){
+    }
+    public function export()
+    {
         $this->exportExcel(Order::all(), 'danhsachdonhang');
     }
 }
