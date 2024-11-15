0ef87bc3b0df37d7d7ce014d32d4c8f19c8ea73b
hatt0904 <tranthihaaaa9423@gmail.com>
2024-11-15 20:57:25 +0700

    Cập nhật StatisticOrderThree

--- app/Livewire/StatisticOrderThree.php
+++ app/Livewire/StatisticOrderThree.php
@@ -8,7 +8,37 @@ use Illuminate\Support\Facades\DB;
 
 class StatisticOrderThree extends Component
 {
+    public $orderDataStatisticOrderThree;
+    public $selectedTimeRangeStatisticOrderThree;
 
+
+    public function mount()
+    {
+        $this->updateChartStatisticOrderThree('province');
+    }
+
+    public function updateChartStatisticOrderThree($area)
+    {
+        $this->orderDataStatisticOrderThree = [];
+        $this->selectedTimeRangeStatisticOrderThree = $area;
+
+
+        $datas = Order::join('addresses as a', 'a.user_id', '=', 'orders.user_id')
+            ->select('a.' . $area, DB::raw('COUNT(orders.id) as count'))
+            ->groupBy('a.' . $area)
+            ->orderByDesc(DB::raw('COUNT(orders.id)'))
+            ->get();
+
+
+        $this->orderDataStatisticOrderThree = $datas->take(10)->map(function ($data) use ($area) {
+            return [$data->$area, $data->count];
+        });
+
+
+        $this->dispatch('updateChartStatisticOrderThree', [
+            'orderDataStatisticOrderThree' => $this->orderDataStatisticOrderThree,
+        ]);
+    }
 
     public function render()
     {
