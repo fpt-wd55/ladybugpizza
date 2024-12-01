3ab49921abda8fee085c0c1a9b6601454f5bab44
Luca <blackwhilee04@gmail.com>
2024-12-01 12:40:41 +0700

    Sửa lỗi form thanh toán

--- resources/views/clients/cart/checkout.blade.php
+++ resources/views/clients/cart/checkout.blade.php
@@ -4,22 +4,21 @@
 
 @section('content')
     @if ($cartItems->count() == 0)
-    <div class="min-h-screen p-4 transition md:mx-24 md:p-8 lg:mx-32">
-        <p class="title">THANH TOÁN</p>
-        <div class="card min-h-96 flex flex-col items-center justify-center gap-8 p-4 text-gray-500 md:p-8">
-            @svg('tabler-shopping-bag-exclamation', 'icon-xl')
-            <p class="text text-center">Giỏ hàng của bạn đang trống</p>
-            <a class="button-red" href="{{ route('client.product.menu') }}">Thực đơn</a>
+        <div class="min-h-screen p-4 transition md:mx-24 md:p-8 lg:mx-32">
+            <p class="title">THANH TOÁN</p>
+            <div class="card min-h-96 flex flex-col items-center justify-center gap-8 p-4 text-gray-500 md:p-8">
+                @svg('tabler-shopping-bag-exclamation', 'icon-xl')
+                <p class="text text-center">Giỏ hàng của bạn đang trống</p>
+                <a class="button-red" href="{{ route('client.product.menu') }}">Thực đơn</a>
+            </div>
         </div>
-    </div>
     @else
-        <form action="{{ route('post-checkout') }}" class="col-span-3" id="form-checkout" method="post">
-            @csrf
-            <div class="min-h-screen">
-                <div class="min-h-screen p-4 transition md:mx-24 md:p-8 lg:mx-32">
-                    <div class="mb-12 p-4 md:p-8">
-                        <div class="grid grid-cols-1 gap-8 lg:grid-cols-5">
-
+        <div class="min-h-screen">
+            <div class="min-h-screen p-4 transition md:mx-24 md:p-8 lg:mx-32">
+                <div class="mb-12 p-4 md:p-8">
+                    <div class="grid grid-cols-1 gap-8 lg:grid-cols-5">
+                        <form action="{{ route('post-checkout') }}" class="col-span-3" id="form-checkout" method="post">
+                            @csrf
                             <div class="col-span-5 lg:col-span-3">
                                 {{-- Thông tin thanh toán --}}
                                 <div class="mb-5 rounded-md border p-5">
@@ -27,7 +26,8 @@
                                     </p>
                                     <div class="mb-4">
                                         <p class="mb-2 text-sm font-normal">Họ và tên: </p>
-                                        <input class="input w-full" name="fullname" placeholder="Họ và tên" type="text" value="{{ old('fullname') ?? Auth::user()->fullname }}">
+                                        <input class="input w-full" name="fullname" placeholder="Họ và tên" type="text"
+                                            value="{{ old('fullname') ?? Auth::user()->fullname }}">
                                         @error('fullname')
                                             <p class="pt-2 text-sm text-red-600">{{ $message }}</p>
                                         @enderror
@@ -35,14 +35,16 @@
                                     <div class="mb-4 grid gap-4 md:grid-cols-2">
                                         <div>
                                             <p class="mb-2 text-sm font-normal">Email:</p>
-                                            <input class="input w-full" name="email" placeholder="Email" type="text" value="{{ old('email') ?? Auth::user()->email }}">
+                                            <input class="input w-full" name="email" placeholder="Email" type="text"
+                                                value="{{ old('email') ?? Auth::user()->email }}">
                                             @error('email')
                                                 <p class="pt-2 text-sm text-red-600">{{ $message }}</p>
                                             @enderror
                                         </div>
                                         <div>
                                             <p class="mb-2 text-sm font-normal">Số điện thoại:</p>
-                                            <input class="input w-full" name="phone" placeholder="Số điện thoại" type="text" value="{{ old('phone') ?? Auth::user()->phone }}">
+                                            <input class="input w-full" name="phone" placeholder="Số điện thoại"
+                                                type="text" value="{{ old('phone') ?? Auth::user()->phone }}">
                                             @error('phone')
                                                 <p class="pt-2 text-sm text-red-600">{{ $message }}</p>
                                             @enderror
@@ -72,7 +74,8 @@
                                     <div class="mb-4">
                                         <p class="mb-2 text-sm font-normal">Địa chỉ chi tiết:
                                         </p>
-                                        <input class="input w-full" name="detail_address" placeholder="Địa chỉ chi tiết" type="text" value="{{ old('detail_address') }}">
+                                        <input class="input w-full" name="detail_address" placeholder="Địa chỉ chi tiết"
+                                            type="text" value="{{ old('detail_address') }}">
                                         @error('detail_address')
                                             <p class="pt-2 text-sm text-red-600">{{ $message }}</p>
                                         @enderror
@@ -80,25 +83,57 @@
                                     <div class="mb-4 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
                                         <div>
                                             <p class="mb-2 text-sm font-normal">Tỉnh/Thành phố:</p>
-                                            <input class="input w-full" name="province" placeholder="Tỉnh/Thành phố" type="text" value="{{ old('province') }}">
+                                            <input class="input w-full" name="province" placeholder="Tỉnh/Thành phố"
+                                                type="text" value="{{ old('province') }}">
                                             @error('province')
                                                 <p class="pt-2 text-sm text-red-600">{{ $message }}</p>
                                             @enderror
                                         </div>
                                         <div>
                                             <p class="mb-2 text-sm font-normal">Quận/Huyện:</p>
-                                            <input class="input w-full" name="district" placeholder="Quận/Huyện" type="text" value="{{ old('district') }}">
+                                            <input class="input w-full" name="district" placeholder="Quận/Huyện"
+                                                type="text" value="{{ old('district') }}">
                                             @error('district')
                                                 <p class="pt-2 text-sm text-red-600">{{ $message }}</p>
                                             @enderror
                                         </div>
                                         <div>
                                             <p class="mb-2 text-sm font-normal">Phường/Xã:</p>
-                                            <input class="input w-full" name="ward" placeholder="Phường/Xã" type="text" value="{{ old('ward') }}">
+                                            <input class="input w-full" name="ward" placeholder="Phường/Xã"
+                                                type="text" value="{{ old('ward') }}">
                                             @error('ward')
                                                 <p class="pt-2 text-sm text-red-600">{{ $message }}</p>
                                             @enderror
                                         </div>
+                                    </div>
+                                </div>
+                                {{-- Thanh toán --}}
+                                <div class="mb-5 rounded-md border p-5">
+                                    <p class="mb-2 font-bold">Phương thức thanh toán</p>
+                                    @error('payment_method_id')
+                                        <p class="mb-2 text-sm text-red-600">{{ $message }}</p>
+                                    @enderror
+                                    @foreach ($paymentMethods as $method)
+                                        <div class="flex items-center justify-between">
+                                            <div class="mb-4 flex items-center">
+                                                <input {{ old('payment_method_id') == $method->id ? 'checked' : '' }}
+                                                    class="input-radio me-2" id="payment_method_id_{{ $method->id }}"
+                                                    name="payment_method_id" type="radio" value="{{ $method->id }}">
+                                                <label class="text-sm font-normal"
+                                                    for="payment_method_id_{{ $method->id }}">{{ $method->name }}</label>
+                                            </div>
+                                            {{-- neu la vnpay thi hien thi credict card --}}
+                                            @if ($method->id == 1)
+                                                @svg('tabler-credit-card', 'icon-lg text-gray-700')
+                                            @else
+                                                @svg('tabler-truck', 'icon-lg text-gray-700')
+                                            @endif
+                                        </div>
+                                        <hr class="mb-4">
+                                    @endforeach
+                                    <div class="flex items-center gap-2 text-sm">
+                                        <span>Bạn cần trợ giúp?</span>
+                                        <a class="link-md" href="{{ route('client.contact') }}">Liên hệ với chúng tôi</a>
                                     </div>
                                 </div>
 
@@ -109,10 +144,13 @@
                                         <!-- component -->
                                         @foreach ($cartItems as $item)
                                             <div class="flex items-start justify-start">
-                                                <img class="h-16 w-16 rounded-md bg-slate-300 object-cover" loading="lazy" onerror="this.src='{{ asset('storage/uploads/products/product-placehoder.jpg') }}'" src="{{ asset('storage/uploads/products/' . $item->product->image) }}">
+                                                <img class="h-16 w-16 rounded-md bg-slate-300 object-cover" loading="lazy"
+                                                    onerror="this.src='{{ asset('storage/uploads/products/product-placehoder.jpg') }}'"
+                                                    src="{{ asset('storage/uploads/products/' . $item->product->image) }}">
                                                 <div class="ms-2 flex w-full flex-col justify-between">
                                                     <div class="item-center mb-1 flex justify-between">
-                                                        <p class="text-sm font-semibold">{{ $item->product->name }}<span class="ps-2 text-sm font-normal">
+                                                        <p class="text-sm font-semibold">{{ $item->product->name }}<span
+                                                                class="ps-2 text-sm font-normal">
                                                                 x{{ $item->quantity }}
                                                             </span></p>
                                                         <p class="font-semibold">{{ number_format($item->price) }}₫</p>
@@ -134,45 +172,20 @@
                                     </div>
                                 </div>
                             </div>
+                        </form>
+                        {{-- Thôn tin đơn hàng - mã giảm giá --}}
+                        <div class="col-span-5 lg:col-span-2">
+                            @livewire('apply-promotion')
 
-                            {{-- Thôn tin đơn hàng - mã giảm giá --}}
-                            <div class="col-span-5 lg:col-span-2">
-                                @livewire('apply-promotion')
-                                {{-- Thanh toán --}}
-                                <div class="mb-5 rounded-md border p-5">
-                                    <p class="mb-2 font-bold">Phương thức thanh toán</p>
-                                    @error('payment_method_id')
-                                        <p class="mb-2 text-sm text-red-600">{{ $message }}</p>
-                                    @enderror
-                                    @foreach ($paymentMethods as $method)
-                                        <div class="flex items-center justify-between">
-                                            <div class="mb-4 flex items-center">
-                                                <input {{ old('payment_method_id') == $method->id ? 'checked' : '' }} class="input-radio me-2" id="payment_method_id_{{ $method->id }}" name="payment_method_id" type="radio" value="{{ $method->id }}">
-                                                <label class="text-sm font-normal" for="payment_method_id_{{ $method->id }}">{{ $method->name }}</label>
-                                            </div>
-                                            {{-- neu la vnpay thi hien thi credict card --}}
-                                            @if ($method->id == 1)
-                                                @svg('tabler-credit-card', 'icon-lg text-gray-700')
-                                            @else
-                                                @svg('tabler-truck', 'icon-lg text-gray-700')
-                                            @endif
-                                        </div>
-                                        <hr class="mb-4">
-                                    @endforeach
-                                    <div class="flex items-center gap-2 text-sm">
-                                        <span>Bạn cần trợ giúp?</span>
-                                        <a class="link-md" href="{{ route('client.contact') }}">Liên hệ với chúng tôi</a>
-                                    </div>
-                                </div>
-                                <div class="flex items-center justify-end">
-                                    <button class="button-red" id="btn-checkout">Thanh toán</button>
-                                </div>
+                            <div class="flex items-center justify-end">
+                                <button class="button-red" id="btn-checkout">Thanh toán</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </form>
+        </div>
+
     @endif
 @endsection
 @section('scripts')
