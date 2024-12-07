af00ecdfff30764077357a49ba90c167cb8240a8
Huuduy2004 <duynguyenhuu2004@gmail.com>
2024-12-07 10:52:54 +0700

    fix validate mã giảm giá

--- app/Http/Requests/PromotionRequest.php
+++ app/Http/Requests/PromotionRequest.php
@@ -34,10 +34,11 @@ class PromotionRequest extends FormRequest
     {
         return [
             'code' => 'required|max:10|unique:promotions,code',
-            'points' => 'required|numeric|min:1',
+            'points' => 'required|numeric|min:0',
             'discount_type' => 'required|numeric|in:1,2',
             'discount_value' => [
                 'required',
+                'min:0',
                 'integer',
                 function ($attribute, $value, $fail) {
                     if ($this->input('discount_type') === '1' && $value > 100) {
@@ -47,9 +48,9 @@ class PromotionRequest extends FormRequest
             ],
             'start_date' => ['required', 'before_or_equal:end_date'],
             'end_date' => ['required', 'after_or_equal:start_date'],
-            'quantity' => 'required|integer|min:1',
-            'min_order_total' => 'nullable|numeric',
-            'max_discount' => 'nullable|numeric',
+            'quantity' => 'required|integer|min:0',
+            'min_order_total' => 'nullable|numeric|min:0',
+            'max_discount' => 'nullable|numeric|min:0',
             'is_global' => 'required',
             'status' => 'nullable',
         ];
@@ -61,11 +62,12 @@ class PromotionRequest extends FormRequest
         $promotionId = $this->route('promotion') ? $this->route('promotion')->id : null;
         return [
             'code' => 'required|max:10|unique:promotions,code,' . $promotionId,
-            'points' => 'required|numeric|min:1',
+            'points' => 'required|numeric|min:0',
             'discount_type' => 'required|numeric|in:1,2',
             'discount_value' => [
                 'required',
                 'integer',
+                'min:0',
                 function ($attribute, $value, $fail) {
                     if ($this->input('discount_type') === '1' && $value > 100) {
                         $fail('Giá trị giảm giá không được lớn hơn 100%');
@@ -75,8 +77,8 @@ class PromotionRequest extends FormRequest
             'start_date' => ['required', 'before_or_equal:end_date'],
             'end_date' => ['required', 'after_or_equal:start_date'],
             'quantity' => 'required|integer|min:1',
-            'min_order_total' => 'nullable|numeric',
-            'max_discount' => 'nullable|numeric',
+            'min_order_total' => 'nullable|numeric|min:0',
+            'max_discount' => 'nullable|numeric|min:0',
             'is_global' => 'required',
             'status' => 'nullable',
         ];
@@ -92,6 +94,7 @@ class PromotionRequest extends FormRequest
             'points.min' => "Điểm phải lớn hơn 0",
             'points.numeric' => "Điểm phải là một số",
             'discount_type.required' => "Bạn cần chọn loại giảm giá",
+            'discount_value.min' => "Giá trị giảm giá không thể là số âm",
             'discount_value.required' => "Bạn cần nhập giá trị giảm giá",
             'discount_value.integer' => "Giá trị giảm giá không hợp lệ",
             'start_date.required' => 'Bạn cần nhập ngày bắt đầu cho mã giảm giá',
@@ -103,7 +106,9 @@ class PromotionRequest extends FormRequest
             'quantity.required' => 'Bạn cần nhập số lượng cho mã giảm giá',
             'quantity.min' => 'Số lượng tối thiểu phải là 1',
             'min_order_total.numeric' => 'Giá trị nhập không thể là chữ',
+            'min_order_total.min' => 'Giá trị nhập không thể là số âm',
             'max_discount.numeric' => 'Giá trị nhập không thể là chữ ',
+            'max_discount.min' => 'Giá trị nhập không thể là số âm',
             'is_global.required' => 'Bạn cần chọn đối tượng áp dụng cho mã giảm giá',
         ];
     }
