6ca0e9bbac3bfd550d367e4d6c8c66336ef2b1fe
Luca <blackwhilee04@gmail.com>
2024-12-07 16:15:49 +0700

    fix UI statistic

--- resources/views/livewire/statistic-order-two.blade.php
+++ resources/views/livewire/statistic-order-two.blade.php
@@ -1,10 +1,10 @@
 <div class="rounded-lg border border-gray-200 bg-white p-4 shadow-sm sm:p-6">
     <div class="w-full">
-        <div class="grid grid-cols-1 lg:grid-cols-3 items-center justify-between pb-4">
-            <div class="w-full col-span-2">
+        <div class="grid grid-cols-1 items-center justify-between pb-4">
+            <div class="w-full">
                 <h3 class="font-bold leading-none text-gray-900 text-lg">Tỷ lệ thanh toán theo phương thức</h3>
             </div>
-            <div class="flex items-center justify-start lg:justify-end w-full md:w-auto mt-3 lg:m-0">
+            <div class="flex items-center justify-start w-full mt-3">
                 <div class="inline-flex items-center rounded-md bg-slate-100 p-1.5">
                     <button wire:click.prevent="updateChartStatisticOrderTwo('week')"
                         class="rounded {{ $selectedTimeRangeStatisticOrderTwo == 'week' ? 'bg-white' : '' }} px-3 py-1 text-xs font-medium text-black shadow-card hover:bg-white me-1">
