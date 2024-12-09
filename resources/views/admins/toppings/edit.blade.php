ef325903341c9579ced35a1fde93f3acdff709f0
hieukhocnhe <trantrunghieu422@gmail.com>
2024-12-09 14:20:14 +0700

    Cập nhật topping admin

--- resources/views/admins/toppings/edit.blade.php
+++ resources/views/admins/toppings/edit.blade.php
@@ -9,32 +9,31 @@
             @csrf
             @method('PUT')
             <div class="flex justify-between gap-2">
-                <div class="w-full h-10 mb-5">
-                    <label for="name" class="label-lg">Tên</label>
+                <div class="w-full mb-5">
+                    <label for="name" class="label-lg">Tên thuộc tính <span class="text-red-500">*</span></label>
                     <input type="text" name="name" class="input mb-2" value="{{ $editTopping->name }}" />
                     @error('name')
                         <span style="color: red">{{ $message }}</span>
                     @enderror
                 </div>
-                <div class="w-full h-10 mb-5">
-                    <label for="name" class="label-lg">Số lượng</label>
+                <div class="w-full mb-5">
+                    <label for="name" class="label-lg">Số lượng <span class="text-red-500">*</span></label>
                     <input type="number" name="quantity" class="input mb-2" value="{{ $editTopping->quantity }}" />
                     @error('quantity')
                         <span style="color: red">{{ $message }}</span>
                     @enderror
                 </div>
-              
             </div>
-            <div class="flex justify-between gap-2 mt-10 mb-10">
-                <div class="w-full h-10 mb-5">
-                    <label for="price" class="label-lg">Giá</label>
+            <div class="flex justify-between gap-2 mt-4">
+                <div class="w-full mb-5">
+                    <label for="price" class="label-lg">Giá <span class="text-red-500">*</span></label>
                     <input type="number" name="price" class="input mb-2" value="{{ $editTopping->price }}" />
                     @error('price')
                         <span style="color: red">{{ $message }}</span>
                     @enderror
                 </div>
-                <div class="w-full h-10 mb-5">
-                    <label for="category_id" class="label-lg">Danh mục</label>
+                <div class="w-full mb-5">
+                    <label for="category_id" class="label-lg">Danh mục <span class="text-red-500">*</span></label>
                     <select class="select w-full mb-2" name="category_id">
                         <option value="">Chọn </option>
                         @foreach ($categories as $category)
@@ -47,29 +46,37 @@
                         <span style="color: red">{{ $message }}</span>
                     @enderror
                 </div>
-               
+
             </div>
-             <div class="w-full h-10 mb-28">
-                    <label class="label-lg">Ảnh</label>
-                    <div>
-                        <input
-                            class="mb-2 block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none "
-                            type="file" name="image">
-                        <a class="shrink-0" data-fslightbox="gallery"
-                            href="{{ asset('storage/uploads/toppings/' . $editTopping->image) }}">
-                            <img loading="lazy" src="{{ asset('storage/uploads/toppings/' . $editTopping->image) }}"
-                                class="img-sm mt-2 img-circle object-cover">
-                        </a>
-                    </div>
-                    @error('image')
-                        <span style="color: red">{{ $message }}</span>
-                    @enderror
+            <label for="name" class="label-lg">Ảnh thuộc tính <span class="text-red-500">*</span></label>
+            <div class="flex gap-3">
+                <a class="shrink-0" data-fslightbox="gallery"
+                    href="{{ asset('storage/uploads/toppings/' . $editTopping->image) }}">
+                    <img loading="lazy" class="w-20 h-20 rounded-md object-cover"
+                        src="{{ asset('storage/uploads/toppings/' . $editTopping->image) }}">
+                </a>
+                <div class="flex items-center justify-center w-full mb-4">
+                    <label for="dropzone-file"
+                        class="flex flex-col items-center justify-center w-full h-20 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
+                        <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                            @svg('tabler-cloud-upload', 'w-8 h-8 text-gray-400 mb-2')
+                            <p class="mb-2 text-sm text-gray-500"><span class="font-semibold">Nhấn để tải
+                                    lên</span>
+                                hoặc kéo thả
+                                vào đây
+                            </p>
+                        </div>
+                        <input id="dropzone-file" name="image" type="file" class="hidden" />
+                    </label>
                 </div>
+            </div>
+            @error('image')
+                <span style="color: red">{{ $message }}</span>
+            @enderror
             <div
                 class="mb-4 flex flex-col flex-shrink-0 space-y-3 md:flex-row md:items-center lg:justify-end md:space-y-0 md:space-x-3">
                 <button class="button-blue">Cập Nhật</button>
-                <a href="{{ route('admin.toppings.index') }}"
-                    class="flex items-center justify-center px-4 py-2 text-sm text-white rounded-lg bg-green-700 hover:bg-green-800 focus:ring-0">Quay
+                <a href="{{ route('admin.toppings.index') }}" class="button-gray">Quay
                     lại</a>
             </div>
         </form>
